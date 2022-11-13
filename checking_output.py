052dcdcd9c0bb6cf416c36d3ac6ddb2d797ad0ce
ABHISHEK CHAKRABORTY <85876018+Chakraborty98@users.noreply.github.com>
2022-11-13 15:41:21 +0530

    Update checking_output.py

--- checking_output.py
+++ checking_output.py
@@ -22,7 +22,7 @@ def prob_viz(res, actions, input_frame, colors,threshold):
     return output_frame
 
 
-# 1. New detection variables
+# New detection variables
 sequence = []
 sentence = []
 accuracy=[]
@@ -30,28 +30,18 @@ predictions = []
 threshold = 0.8 
 
 cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture("https://192.168.43.41:8080/video")
-# Set mediapipe model 
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
 
-        # Read feed
-        ret, frame = cap.read()
-
-        # Make detections
-        cropframe=frame[40:400,0:300]
-        # print(frame.shape)
+        ret, frame = cap.read()        
+        cropframe=frame[40:400,0:300]        
         frame=cv2.rectangle(frame,(0,40),(300,400),(0,255,0),2)
-        # frame=cv2.putText(frame,"Active Region",(75,25),cv2.FONT_HERSHEY_COMPLEX_SMALL,2,255,2)
         image, results = mediapipe_detection(cropframe, hands)
-        # print(results)
-        
-        # Draw landmarks
-        # draw_styled_landmarks(image, results)
-        # 2. Prediction logic
+       
+# Prediction logic
         keypoints = extract_keypoints(results)
         sequence.append(keypoints)
         sequence = sequence[-30:]
@@ -61,9 +51,7 @@ with mp_hands.Hands(
                 res = model.predict(np.expand_dims(sequence, axis=0))[0]
                 print(actions[np.argmax(res)])
                 predictions.append(np.argmax(res))
-                
-                
-            #3. Viz logic
+   
                 if np.unique(predictions[-10:])[0]==np.argmax(res): 
                     if res[np.argmax(res)] > threshold: 
                         if len(sentence) > 0: 
@@ -77,21 +65,16 @@ with mp_hands.Hands(
                 if len(sentence) > 1: 
                     sentence = sentence[-1:]
                     accuracy=accuracy[-1:]
-
-                # Viz probabilities
-                # frame = prob_viz(res, actions, frame, colors,threshold)
+                
         except Exception as e:
-            # print(e)
             pass
             
         cv2.rectangle(frame, (0,0), (300, 40), (0, 0, 255), -1)
         cv2.putText(frame,"Output: -"+' '.join(sentence)+''.join(accuracy), (3,30), 
                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
         
-        # Show to screen
         cv2.imshow('OpenCV Feed', frame)
 
-        # Break gracefully
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
     cap.release()
